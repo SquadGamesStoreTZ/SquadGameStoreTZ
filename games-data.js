106760620529da633d43643037cd9b839a433adb
SquadGameDealer <iamsquardii@gmail.com>
2026-09-04 19:48:13 +0300

    Update games-data.js

--- games-data.js
+++ games-data.js
@@ -1,39 +1,68 @@
-var _0x5d12=['https://wa.me/255692752060?text=Hello%20Squad%20Games%2C%20I%20want%20to%20buy%20Euro%20Truck%20Simulator%202%20v1.57.2.2s%20%2B%20103%20DLCs%20%2B%20Multiplayer%20Game%20(TZS%205%2C000).','images/ets-2-pc.jpg','images/Screenshot_20260902_131118_TikTok.jpg','images/Annotation\x202026-09-02\x20130017.png','images/Annotation\x202026-09-02\x20125943.png','Euro\x20Truck\x20Simulator\x202\x20%2B\x2050\x20TZ\x20Mods\x20Pack','images/4193b766a912970fac32e8b171d693df.webp','images/f645a854f358ea2a930c5be36d485616.webp','images/be9b02184c2d31b4c53e8500366db61e.webp','images/Annotation\x202026-09-02\x20125957.png','images/ets-2-mobile.jpg','images/Screenshot_20260902_131001_TikTok.jpg','images/Screenshot_20260902_130935_TikTok.jpg','images/ets\x202\x201.57.png'];
+(function (_0x1a2b3c, _0x4d5e6f) {
+    var _0x3b2a = function (_0x2c1d0e) {
+        while (--_0x2c1d0e) {
+            _0x1a2b3c['push'](_0x1a2b3c['shift']());
+        }
+    };
+    _0x3b2a(++_0x4d5e6f);
+}(_0x5d12 = [
+    'https://wa.me/255692752060?text=Hello%20Squad%20Games%2C%20I%20want%20to%20buy%20Euro%20Truck%20Simulator%202%20v1.57.2.2s%20%2B%20103%20DLCs%20%2B%20Multiplayer%20Game%20(TZS%205%2C000).',
+    'images/ets-2-pc.jpg',
+    'images/Screenshot_20260902_131118_TikTok.jpg',
+    'images/Annotation\x202026-09-02\x20130017.png',
+    'images/Annotation\x202026-09-02\x20125943.png',
+    'Euro\x20Truck\x20Simulator\x202\x20%2B\x2050\x20TZ\x20Mods\x20Pack',
+    'images/4193b766a912970fac32e8b171d693df.webp',
+    'images/f645a854f358ea2a930c5be36d485616.webp',
+    'images/be9b02184c2d31b4c53e8500366db61e.webp',
+    'images/Annotation\x202026-09-02\x20125957.png',
+    'images/ets-2-mobile.jpg',
+    'images/Screenshot_20260902_131001_TikTok.jpg',
+    'images/Screenshot_20260902_130935_TikTok.jpg',
+    'images/ets\x202\x201.57.png'
+], 0x1f4));
+
+var _0x2a1b = function (_0x1a2b3c, _0x4d5e6f) {
+    _0x1a2b3c = _0x1a2b3c - 0x0;
+    var _0x3b2a = _0x5d12[_0x1a2b3c];
+    return _0x3b2a;
+};
+
 const gamesData = [
     {
-        id: 1,
-        title: "Euro Truck Simulator 2 + 50 TZ Mods Pack",
-        platform: "PC",
-        category: "Simulation / PC",
-        description: "Full Euro Truck Simulator 2 PC game bundled with 50 custom TZ mods.",
-        requirements: "OS: Windows 10/11 (64-bit) | RAM: 8 GB | Storage: 25 GB | GPU: Intel HD 620 / NVIDIA GTX 660",
-        price: "TZS 30,000",
-        image: _0x5d12[1],
-        screenshots: [_0x5d12[6], _0x5d12[7], _0x5d12[2]],
-        downloadUrl: "https://wa.me/255692752060?text=Hello%20Squad%20Games%2C%20I%20want%20to%20buy%20Euro%20Truck%20Simulator%202%20%2B%2050%20TZ%20Mods%20Pack%20(TZS%2030%2C000)."
+        'id': 1,
+        'title': 'Euro Truck Simulator 2 + 50 TZ Mods Pack',
+        'platform': 'PC',
+        'category': 'Simulation / PC',
+        'description': 'Full Euro Truck Simulator 2 PC game bundled with 50 custom TZ mods.',
+        'requirements': 'OS: Windows 10/11 (64-bit) | RAM: 8 GB | Storage: 25 GB | GPU: Intel HD 620 / NVIDIA GTX 660',
+        'price': 'TZS 30,000',
+        'image': _0x2a1b('0x1'),
+        'screenshots': [_0x2a1b('0x6'), _0x2a1b('0x7'), _0x2a1b('0x2')],
+        'downloadUrl': 'https://wa.me/255692752060?text=Hello%20Squad%20Games%2C%20I%20want%20to%20buy%20Euro%20Truck%20Simulator%202%20%2B%2050%20TZ%20Mods%20Pack%20(TZS%2030%2C000).'
     },
     {
-        id: "ets2-mobile",
-        title: "Euro Truck Simulator 2 Mobile TZ",
-        category: "Mobile Games",
-        platform: "Android",
-        description: "Experience driving heavy trucks across Tanzania directly on your Android phone.",
-        requirements: "OS: Android 8.0+ | RAM: 4 GB minimum | Storage: 3 GB free space",
-        price: "TZS 15,000",
-        image: _0x5d12[10],
-        screenshots: [_0x5d12[11], _0x5d12[12], _0x5d12[8]],
-        downloadUrl: "https://wa.me/255692752060?text=Hello%20Squad%20Games%2C%20I%20want%20to%20buy%20Euro%20Truck%20Simulator%202%20Mobile%20TZ%20(TZS%2015%2C000)."
+        'id': 'ets2-mobile',
+        'title': 'Euro Truck Simulator 2 Mobile TZ',
+        'category': 'Mobile Games',
+        'platform': 'Android',
+        'description': 'Experience driving heavy trucks across Tanzania directly on your Android phone.',
+        'requirements': 'OS: Android 8.0+ | RAM: 4 GB minimum | Storage: 3 GB free space',
+        'price': 'TZS 15,000',
+        'image': _0x2a1b('0xa'),
+        'screenshots': [_0x2a1b('0xb'), _0x2a1b('0xc'), _0x2a1b('0x8')],
+        'downloadUrl': 'https://wa.me/255692752060?text=Hello%20Squad%20Games%2C%20I%20want%20to%20buy%20Euro%20Truck%20Simulator%202%20Mobile%20TZ%20(TZS%2015%2C000).'
     },
     {
-        id: "ets2-v157",
-        title: "Euro Truck Simulator 2 v1.57.2.2s + 103 DLCs + Multiplayer Game",
-        platform: "PC",
-        category: "Open World / Simulation",
-        description: "It includes 103 DLCs + Multiplayer Game",
-        requirements: "OS: Windows 7/8.1/10/11 (64-bit) | RAM: 8 GB | Storage: 25 GB | GPU: Nvidia GTX 660 / AMD HD 7870",
-        price: "TZS 5,000",
-        image: _0x5d12[13],
-        screenshots: [_0x5d12[9], _0x5d12[3], _0x5d12[4]],
-        downloadUrl: _0x5d12[0]
+        'id': 'ets2-v157',
+        'title': 'Euro Truck Simulator 2 v1.57.2.2s + 103 DLCs + Multiplayer Game',
+        'platform': 'PC',
+        'category': 'Open World / Simulation',
+        'description': 'It includes 103 DLCs + Multiplayer Game',
+        'requirements': 'OS: Windows 7/8.1/10/11 (64-bit) | RAM: 8 GB | Storage: 25 GB | GPU: Nvidia GTX 660 / AMD HD 7870',
+        'price': 'TZS 5,000',
+        'image': _0x2a1b('0xd'),
+        'screenshots': [_0x2a1b('0x9'), _0x2a1b('0x3'), _0x2a1b('0x4')],
+        'downloadUrl': _0x2a1b('0x0')
     }
 ];
